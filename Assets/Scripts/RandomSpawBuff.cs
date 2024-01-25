e94a0a0614e71e5a9f0687df8325448e367b71cd
Aphichat0020 <65810266+Aphichat0020@users.noreply.github.com>
2024-01-25 15:37:43 +0700

    Update _Spaw_Buff

---
+++ Assets/Scripts/RandomSpawBuff.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawBuff : MonoBehaviour
+{
+    public Transform[] PositionBuff;
+    public GameObject[] ObjectBuff;
+
+    public GameObject IsBuffinGameNow;
+
+    public float TimeChangBuff;
+   // private Transform LocationBuff;
+    void Start()
+    {
+        Spaw_Buff();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Spaw_Buff();
+        }
+    }
+    public void Spaw_Buff()
+    {
+        // PositionBuff = PositionBuff[Random.Range(0, PositionBuff.Length)];
+        IsBuffinGameNow = Instantiate(ObjectBuff[Random.Range(0, ObjectBuff.Length)],PositionBuff[Random.Range(0, PositionBuff.Length)]);
+      // StartCoroutine(TimeDestroyBuff());
+    }
+    public void GetBuff()
+    {
+        Destroy(IsBuffinGameNow);
+        //Player have buff
+        // 
+    }
+    IEnumerator TimeDestroyBuff()
+    {
+        yield return new WaitForSeconds(TimeChangBuff);
+        Destroy(IsBuffinGameNow);
+       // Spaw_Buff();
+    }
+
+}
