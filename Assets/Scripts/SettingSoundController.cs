2f72f65d2e2c21e6f744b8cb14c0f272fde9ece6
Aphichat0020 <65810266+Aphichat0020@users.noreply.github.com>
2024-01-27 08:45:47 +0700

    MixScene

--- Assets/Scripts/SettingSoundController.cs
+++ Assets/Scripts/SettingSoundController.cs
@@ -6,7 +6,18 @@ using UnityEngine.UI;
 public class SettingSoundController : MonoBehaviour
 {
    public Slider _MusicSlider, _SFXSlider;
-
+    public GameObject SettingUI;
+    public void Start()
+    {
+        
+    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SettingUI.SetActive(true);
+        }
+    }
     public void ToggleMusic()
     {
         AudioManager_New.instance.ToggleMusic();
