2ad50d4e140e86d9eb27cd1d6f2e1843f096683d
Aphichat0020 <65810266+Aphichat0020@users.noreply.github.com>
2024-01-27 09:58:40 +0700

    UpdateMusic_Update_SettingUI

--- Assets/Scripts/SettingSoundController.cs
+++ Assets/Scripts/SettingSoundController.cs
@@ -1,22 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingSoundController : MonoBehaviour
 {
    public Slider _MusicSlider, _SFXSlider;
     public GameObject SettingUI;
-    public void Start()
+
+    [SerializeField] AudioMixer mixer;
+
+    public const string MIXER_MUSIC = "MusicVolume";
+    public const string MIXER_SFX = "SFXVolume";
+
+    void Awake()
     {
-        
+        _MusicSlider.onValueChanged.AddListener(SetMusicVolume);
+        _SFXSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+    void Start()
+    {
+        _MusicSlider.value = PlayerPrefs.GetFloat(AudioManager.MUSIC_KEY, 1f);
+        _SFXSlider.value = PlayerPrefs.GetFloat(AudioManager.SFX_KEY, 1f);
+
     }
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        //if(Input.GetKeyUp(KeyCode.Escape))
+        //{
+        //    SettingUI.SetActive(true);
+        //}
+        if (Input.GetKeyDown(KeyCode.Escape) && SettingUI.activeInHierarchy == false)
         {
             SettingUI.SetActive(true);
         }
+        else if(Input.GetKeyDown(KeyCode.Escape) && SettingUI.activeInHierarchy == true)
+        {
+           SettingUI.SetActive(false);
+        }
+    }
+
+    public void OnDisable()
+    {
+        PlayerPrefs.SetFloat(AudioManager.MUSIC_KEY, _MusicSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_KEY, _SFXSlider.value);
+    }
+    public void SetMusicVolume(float value)
+    {
+        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(value) * 20);
+    }
+    public void SetSFXVolume(float value)
+    {
+        mixer.SetFloat(MIXER_SFX, Mathf.Log10(value) * 20);
     }
     public void ToggleMusic()
     {
@@ -35,3 +71,4 @@ public class SettingSoundController : MonoBehaviour
         AudioManager_New.instance.SFXVolume(_SFXSlider.value);
     }
 }
+
